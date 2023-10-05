418048702fc8f167e40da65844265bee86e62877
khushi303 <maankhushi303@gmail.com>
2023-10-05 10:22:59 +0530

    taekook

--- assets/jsfile/index.js
+++ assets/jsfile/index.js
@@ -16,14 +16,14 @@
 // console.log(d)
 // console.log(d.b)
 //4
-var smile = "cake"
-console.log(smile)
-//5
-var is = "is"
-console.log(is)
-//6
-var beautiful = "sweat"
-console.log(beautiful)
+// var smile = "cake"
+// console.log(smile)
+// //5
+// var is = "is"
+// console.log(is)
+// //6
+// var beautiful = "sweat"
+// console.log(beautiful)
 //7
 // function fun() {
 //     var abc = 123
@@ -120,7 +120,7 @@ console.log(beautiful)
 // console.log(c);
 // console.log(b);
 //dom
-document.getElementById("sec-1").classList.add("anime1");
+// document.getElementById("sec-1").classList.add("anime1");
 // console.log(document.getElementById("sec-1"));
 // console.log(document.getElementsByClassName("anime1"));
 // document.getElementById("hi").style.background = "red";
@@ -168,18 +168,120 @@ document.getElementById("sec-1").classList.add("anime1");
 //         break;
 // }
 
-function hii() {
-    document.getElementById("hi").innerHTML = Date();
-}
+// function hii() {
+//     document.getElementById("hi").innerHTML = Date();
+// }
 
-let btn = document.getElementById("btn");
-let onbtn = document.getElementById("onbtn");
-let offbtn = document.getElementById("offbtn");
+// let btn = document.getElementById("btn");
+// let onbtn = document.getElementById("onbtn");
+// let offbtn = document.getElementById("offbtn");
 
-onbtn.addEventListener("click", function () {
-    btn.classList.add("bg_yellow")
-})
+// onbtn.addEventListener("click", function () {
+//     btn.classList.add("bg_yellow")
+// })
 
-offbtn.addEventListener("click", function () {
-    btn.classList.remove("bg_yellow")
-})
+// offbtn.addEventListener("click", function () {
+//     btn.classList.remove("bg_yellow")
+// })
+
+// function myname() {
+//     var p = document.getElementById("pwds")
+//     if (p.type === "password") {
+//         p.type = "text";
+//     } else {
+//         p.type = "password";
+//     }
+// }
+// var a = [1, 3, 4, 5, 7, 3, 7, 4, 3, 2]
+// var b = a.filter((e) => e < 2)
+// console.log(b);
+
+//---------------------string-method-----------------------
+// slice(1);
+let ant = "khushi,tarun,sakshi";
+let bat = ant.slice(7);
+console.log(bat);
+
+//charAt(2);
+let cat = "khushi,tarun,sakshi";
+let dog = cat.charAt(5);
+console.log(dog);
+
+//concat(3);
+let egg = "khushi,tarun,sakshi";
+let fog = egg.concat(" " + "alka");
+console.log(fog);
+
+//indexof(4);
+let goat = ["khushi", "tarun", "sakshi"];
+console.log(goat.indexOf("khushi"));
+
+//startwith(5);
+let hot = "this is khushi"
+console.log(hot.startsWith("this"));
+
+//endwith(6);
+let ink = "this is khushi"
+console.log(ink.endsWith("khushi"));
+
+//split(7);
+let jk = "1,2,3,4";
+let kh = jk.split("3");
+console.log(kh);
+
+//-----------------------------number------------------------------------//
+//parsent();  =  change no. to string
+
+
+//tofixed(); = print decimal no.
+let loki = 24.4556543433
+console.log(loki.toFixed(4));
+
+//toprecision(); = print the no. and decimal no. both
+let min = 2343.65554;
+console.log(min.toPrecision(5));
+
+//tostring(); = change no. in string
+let namjoon = 12345;
+let jin = namjoon.toString();
+console.log(typeof jin)
+
+
+//----------------------------------array---------------------------------//
+//filter(); = get new array from existing array by condition
+let okie = [1, 3, 5, 7, 0, 4, 7];
+let dokie = okie.filter((e) => e + 6);
+console.log(dokie);
+
+//map(); = give a new array in boolean
+let pinki = [1, 3, 4, 5, 6, 7, 3, 6, 7, 4];
+let rakhi = pinki.map((s) => s > 5);
+console.log(rakhi);
+
+//find(); = print a no. who satisfied the condition order-first
+let queen = [1, 3, 4, 6, 8, 4, 3, 2];
+let king = queen.find((e) => e > 5);
+console.log(king);
+
+//every(); =🤣😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂//
+
+
+//sort();
+let suga = [1, 2, 3, 2, 4, 2, 4, 2, 4, 8, 2, 4, 5, 5, 3];
+let proud = suga.sort();
+console.log(suga);
+
+//slice();
+let taehyung = [1, 4, 3, 6, 4, 3, 8, 6, 34];
+let jungkookie = taehyung.slice(3, 7);
+console.log(jungkookie);
+
+//splice();
+let unique = []
+
+
+//foreach();
+let v = [1, 3, 2, 4, 5, 3, 5];
+v.forEach((e) => {
+    console.log(e * e);
+});
